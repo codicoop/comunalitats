549e9dcc852819c0fcaf8b257cd277451b4c7b8b
Pere <pere@perepicornell.com>
2018-11-26 17:05:11 +0100

    Specifying time zone and language code.

--- coopolis_backoffice/settings/dev.py
+++ coopolis_backoffice/settings/dev.py
@@ -114,9 +114,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/2.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ca_ES'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Europe/Andorra'
 
 USE_I18N = True
 
