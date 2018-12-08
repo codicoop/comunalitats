465454e75227085769d2d4c325eb804bd1c70330
Alfred <alfredgg@yahoo.es>
2018-12-08 13:22:21 +0100

    Attendee lists in activities and improve its format

--- apps/coopolis/admin/ActivityAdmin.py
+++ apps/coopolis/admin/ActivityAdmin.py
@@ -2,7 +2,39 @@
 # -*- coding: utf-8 -*-
 
 from django.contrib import admin
+from django.utils.html import format_html
+from django.http import HttpResponse
+from django.urls import path, reverse
+from cc_courses.models import Activity
 
 
 class ActivityAdmin(admin.ModelAdmin):
-    pass
+    list_display = ('name', 'my_url_field',)
+
+    def get_urls(self):
+        urls = super().get_urls()
+        custom_urls = [
+            path(
+                r'<_id>/attendee-list/',
+                self.admin_site.admin_view(self.attendee_list),
+                name='attendee-list',
+            ),
+        ]
+        return custom_urls + urls
+
+    def my_url_field(self, obj):
+        return format_html('<a href="%s" target="_new">Llista d\'assistencia</a>' % reverse('admin:attendee-list', kwargs={'_id': obj.id}))
+
+    my_url_field.allow_tags = True
+    my_url_field.short_description = 'Column description'
+
+    def attendee_list(self, request, _id):
+        import weasyprint
+        import django.template.loader as loader
+        temp = loader.get_template('admin/attendee_list.html')
+        content = temp.render({'assistants': Activity.objects.get(pk=_id).enrolled.all()})
+        pdf = weasyprint.HTML(string=content.encode('utf-8'))
+        response = HttpResponse(pdf.write_pdf(), content_type='application/pdf')
+        # response['Content-Disposition'] = 'attachment; filename="llista_assistencia.pdf"'
+        response['Content-Disposition'] = 'filename="llista_assistencia.pdf"'
+        return response
--- apps/coopolis/admin/CourseAdmin.py
+++ apps/coopolis/admin/CourseAdmin.py
@@ -1,41 +1,12 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from django.utils.html import format_html
-from django.http import HttpResponse
-from django.urls import path, reverse
-from cc_courses.models import Course
+
 from django_summernote.admin import SummernoteModelAdmin
 
 
 class CourseAdmin(SummernoteModelAdmin):
-    list_display = ('title', 'my_url_field',)
+    list_display = ('title',)
     summernote_fields = ('objectives',)
 
-    def get_urls(self):
-        urls = super().get_urls()
-        custom_urls = [
-            path(
-                r'<_id>/attendee-list/',
-                self.admin_site.admin_view(self.attendee_list),
-                name='attendee-list',
-            ),
-        ]
-        return custom_urls + urls
 
-    def my_url_field(self, obj):
-        return format_html('<a href="%s" target="_new">Llista d\'assistencia</a>' % reverse('admin:attendee-list', kwargs={'_id': obj.id}))
-
-    my_url_field.allow_tags = True
-    my_url_field.short_description = 'Column description'
-
-    def attendee_list(self, request, _id):
-        import weasyprint
-        import django.template.loader as loader
-        temp = loader.get_template('admin/attendee_list.html')
-        content = temp.render({'assistants': Course.objects.get(pk=_id).enrolled.all()})
-        pdf = weasyprint.HTML(string=content.encode('utf-8'))
-        response = HttpResponse(pdf.write_pdf(), content_type='application/pdf')
-        # response['Content-Disposition'] = 'attachment; filename="llista_assistencia.pdf"'
-        response['Content-Disposition'] = 'filename="llista_assistencia.pdf"'
-        return response
