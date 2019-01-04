3b2e1ad57a081c870cac460e8113436f7a936e22
Pere <pere@perepicornell.com>
2019-01-04 18:34:04 +0100

    Course detail: added list of course activities.

--- apps/cc_courses/views/CourseDetailView.py
+++ apps/cc_courses/views/CourseDetailView.py
@@ -3,9 +3,16 @@
 
 from django.views import generic
 from django.apps import apps
+from cc_courses.models import Course, Activity
 
 
 class CourseDetailView(generic.DetailView):
-    model = apps.get_model('cc_courses', 'Course')
+    model = Course
     template_name = 'course.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['activities'] = Activity.objects.all()[:5]
+        # context['past_courses'] = Course.objects.filter(date_end__lt=timezone.now().date())
+        return context
 
