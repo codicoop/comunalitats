aebf36cff162b098c755dddcfa95c873eee1f4ae
Pere <pere@perepicornell.com>
2019-01-11 19:15:36 +0100

    My Courses: list in view working.

--- apps/cc_courses/views/MyCoursesView.py
+++ apps/cc_courses/views/MyCoursesView.py
@@ -10,3 +10,8 @@ class MyCoursesListView(generic.ListView):
     model = Activity
     template_name = 'my_courses.html'
 
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['enrolled_activities'] = Activity.objects.filter(date_end__gte=timezone.now().date(),
+                                                                 enrolled=self.request.user)
+        return context
