0bf15fcf361d1f7c86fea296759504bd2455e68a
Pere <pere@perepicornell.com>
2019-01-30 12:01:29 +0100

    Courses list now have its own URL + added Home template and temporal content.

--- apps/cc_courses/urls.py
+++ apps/cc_courses/urls.py
@@ -7,9 +7,9 @@ from .utils import get_courses_list_view_class
 
 
 urlpatterns = [
-    path('', get_courses_list_view_class().as_view(), name='courses'),
     path('activities/my_activities', MyCoursesListView.as_view(), name='my_activities'),
     path('program/<slug>', CourseDetailView.as_view(), name='course'),
+    path('program/', get_courses_list_view_class().as_view(), name='courses'),
     path('<id>/enroll', EnrollActivityView.as_view(), name='enroll_course'),
     path('activities/<id>/activity_optout', OptoutActivityView.as_view(), name='activity_optout')
 ]
--- apps/coopolis/urls.py
+++ apps/coopolis/urls.py
@@ -3,10 +3,10 @@
 
 from django.urls import path, include
 from .admin import coopolis_admin_site
-from .views import CoopolisCoursesListView, ProjectFormView, SignUp, LogIn, ProjectCreateFormView
+from .views import ProjectFormView, SignUp, LogIn, ProjectCreateFormView
 from django.conf.urls import url
 from django.conf import settings
-from django.views.generic.base import RedirectView
+from django.views.generic.base import RedirectView, TemplateView
 
 
 urlpatterns = [
@@ -14,12 +14,19 @@ urlpatterns = [
 ]
 
 urlpatterns += [
+    path('', TemplateView.as_view(
+        template_name="home.html",
+        extra_context={
+            # The lambda makes this expression to be executed each call of home (because of the admin updates)
+            'courses_text': "TEXT D'INTRODUCCIÓ A LES FORMACIONS QUE FEM",
+            'projects_text': "Lorem ipsum TEXT D'INTRODUCCIÓ A L'ACOMPANYAMENT DE PROJECTES"
+        }
+    ), name='home'),
     path('login/', LogIn.CoopolisLoginView.as_view(), name='login'),
     path('users/login/', LogIn.CoopolisLoginView.as_view(), name='login'),
     path('signup', SignUp.signup, name='signup'),
     path('admin/', coopolis_admin_site.urls),
     path('summernote/', include('django_summernote.urls')),
-    path('', CoopolisCoursesListView.as_view(), name='home'),
     path('project/', ProjectFormView.as_view(), name='project'),
     path('project/new/', ProjectCreateFormView.as_view(), name='new_project'),
 ]
