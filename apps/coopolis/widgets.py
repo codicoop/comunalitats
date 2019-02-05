cb51d07b0bfba6b7d0fd89d9988c088bab090883
Pere <pere@perepicornell.com>
2019-02-05 18:14:37 +0100

    Datepick fix: prevent field to show cached values dropdown overlapping the datepicker.

--- apps/coopolis/widgets.py
+++ apps/coopolis/widgets.py
@@ -9,10 +9,19 @@ from django.forms import TimeInput
 class XDSoftDateTimePickerInput(DateTimeInput):
     template_name = 'widgets/xdsoft_datetimepicker.html'
 
+    def __init__(self):
+        super().__init__(attrs={'autocomplete': 'off'})
+
 
 class XDSoftDatePickerInput(DateInput):
     template_name = 'widgets/xdsoft_datepicker.html'
 
+    def __init__(self):
+        super().__init__(attrs={'autocomplete': 'off'})
+
 
 class XDSoftTimePickerInput(TimeInput):
     template_name = 'widgets/xdsoft_timepicker.html'
+
+    def __init__(self):
+        super().__init__(attrs={'autocomplete': 'off'})
