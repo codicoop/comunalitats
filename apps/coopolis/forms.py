c52088332dd3ecfc077be861905b020edc83a84f
Pere <pere@perepicornell.com>
2019-02-06 18:26:56 +0100

    Project: registration date not required anymore.

--- apps/coopolis/forms.py
+++ apps/coopolis/forms.py
@@ -8,7 +8,7 @@ from coopolis.widgets import XDSoftDatePickerInput
 
 
 class ProjectForm(forms.ModelForm):
-    registration_date = forms.DateField(widget=XDSoftDatePickerInput())
+    registration_date = forms.DateField(widget=XDSoftDatePickerInput(), required=False)
 
     class Meta:
         model = Project
