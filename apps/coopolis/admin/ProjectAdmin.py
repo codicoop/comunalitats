e349203f0a2016f4a8bce7044057bec6711860a5
Pere <pere@perepicornell.com>
2019-02-13 20:18:00 +0100

    Experimenting with Inlines.

--- apps/coopolis/admin/ProjectAdmin.py
+++ apps/coopolis/admin/ProjectAdmin.py
@@ -4,10 +4,22 @@
 from django.contrib import admin
 from coopolis.models import User
 
+
+class UserInline(admin.StackedInline):
+    model = User
+    fields = ('first_name',)
+    extra = 0
+
+
 class ProjectAdmin(admin.ModelAdmin):
+    inlines = [
+        UserInline,
+    ]
 
     def formfield_for_foreignkey(self, db_field, request, **kwargs):
         if db_field.name == "project_responsible":
             kwargs["queryset"] = User.objects.filter(is_staff=True)
         return super().formfield_for_foreignkey(db_field, request, **kwargs)
 
+    def _users(self, obj):
+        return obj.projects.all().count()
