0d304a2e4ff680c7db9737a7a27d206b735fd5aa
Pere <pere@perepicornell.com>
2019-02-13 18:00:07 +0100

    Some User fields filtered or set to read only

--- apps/coopolis/admin/UserAdmin.py
+++ apps/coopolis/admin/UserAdmin.py
@@ -8,4 +8,6 @@ class UserAdmin(admin.ModelAdmin):
     list_display = ('id_number', 'last_name', 'first_name', 'email', 'project')
     search_fields = ('id_number', 'last_name', 'first_name', 'email', 'residence_town',
                      'residence_district', 'phone_number', 'cooperativism_knowledge', 'adreca_nom_via')
-    list_filter = ('gender', 'project', 'residence_town', 'residence_district')
+    list_filter = ('gender', 'project', 'residence_town', 'residence_district', 'is_staff')
+    exclude = ('user_permissions', 'groups', 'is_confirmed', 'password')
+    readonly_fields = ('last_login',)
