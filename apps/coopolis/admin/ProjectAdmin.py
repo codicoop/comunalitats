42c14bf1f17dbab93f05f8f3867a618d27b969a7
Pere <pere@perepicornell.com>
2019-02-13 17:39:48 +0100

    Project responsible dropdown now only shows staff_member users.

--- apps/coopolis/admin/ProjectAdmin.py
+++ apps/coopolis/admin/ProjectAdmin.py
@@ -2,7 +2,12 @@
 # -*- coding: utf-8 -*-
 
 from django.contrib import admin
-
+from coopolis.models import User
 
 class ProjectAdmin(admin.ModelAdmin):
-    pass
+
+    def formfield_for_foreignkey(self, db_field, request, **kwargs):
+        if db_field.name == "project_responsible":
+            kwargs["queryset"] = User.objects.filter(is_staff=True)
+        return super().formfield_for_foreignkey(db_field, request, **kwargs)
+
