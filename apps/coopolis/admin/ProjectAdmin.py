f26f1970adf072eb1b4a4368a4aefddfb4e28e05
Pere <pere@perepicornell.com>
2019-02-15 19:28:54 +0100

    Project admin: created mixin to export to CSV and applied to Project.

--- apps/coopolis/admin/ProjectAdmin.py
+++ apps/coopolis/admin/ProjectAdmin.py
@@ -2,18 +2,20 @@
 # -*- coding: utf-8 -*-
 
 from django.contrib import admin
-from coopolis.models import User
+from coopolis.models import User, Project
 from simple_history.admin import SimpleHistoryAdmin
 from django.utils.safestring import mark_safe
+from coopolis.mixins import ExportCsvMixin
 
 
-class ProjectAdmin(SimpleHistoryAdmin):
+class ProjectAdmin(SimpleHistoryAdmin, ExportCsvMixin):
     list_display = ('name', 'web', 'mail', 'phone', 'project_responsible', 'registration_date', 'subsidy_period',
                     'partners_field')
     search_fields = ('name', 'web', 'mail', 'phone', 'project_responsible', 'registration_date', 'subsidy_period',
                      'object_finality', 'project_origins', 'solves_necessities', 'social_base', 'sector')
     list_filter = (('project_responsible', admin.RelatedOnlyFieldListFilter), 'registration_date', 'subsidy_period',
                    'sector')
+    actions = ["export_as_csv"]
 
     def formfield_for_foreignkey(self, db_field, request, **kwargs):
         if db_field.name == "project_responsible":
