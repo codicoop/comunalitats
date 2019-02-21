b9b18a46e22b81d4a3e8a71bee56de9499e285d4
Pere <pere@perepicornell.com>
2019-02-21 13:36:03 +0100

    Added "Accept legal conditions" checkbox in signup form.

--- apps/coopolis/forms.py
+++ apps/coopolis/forms.py
@@ -5,7 +5,7 @@ from django import forms
 from coopolis.models import Project, User
 from django.contrib.auth.forms import UserCreationForm
 from coopolis.widgets import XDSoftDatePickerInput
-
+from django.utils.safestring import mark_safe
 
 class ProjectForm(forms.ModelForm):
     required_css_class = "required"
@@ -22,6 +22,9 @@ class MySignUpForm(UserCreationForm):
     last_name = forms.CharField(label="Cognom", max_length=30, required=False)
     email = forms.EmailField(label="Correu electrònic", max_length=254, help_text='Requerit, ha de ser una adreça vàlida.')
     birthdate = forms.DateField(label="Data de naixement", required=False, widget=XDSoftDatePickerInput())
+    accept_conditions = forms.BooleanField(
+        label=mark_safe('Accepto les <a href="https://bcn.coop/avis-legal-i-proteccio-de-dades/" target="_blank">condicions legals</a>'),
+        required=True)
 
     class Meta(UserCreationForm.Meta):
         model = User
