e563feb36550d4d5aac7d4daf0b17314268e1b63
Pere <pere@perepicornell.com>
2019-04-28 21:46:12 +0200

    Fix: duplicated courses in list.

--- apps/cc_courses/views/CoursesListView.py
+++ apps/cc_courses/views/CoursesListView.py
@@ -13,6 +13,7 @@ class CoursesListView(generic.ListView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['future_courses'] = Course.published.filter(activities__date_start__gte=timezone.now().date())
+        context['future_courses'] = Course.published.filter(
+            activities__date_start__gte=timezone.now().date()).distinct()
         # context['future_courses'] = context['course_list']
         return context
