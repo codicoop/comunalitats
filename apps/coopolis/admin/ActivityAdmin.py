2575975a9d40648a1335bb16108aa9c05e4c852f
Pere <pere@perepicornell.com>
2019-06-14 12:31:20 +0200

    AdminAction: Cleaning unused code.

--- apps/coopolis/admin/ActivityAdmin.py
+++ apps/coopolis/admin/ActivityAdmin.py
@@ -37,46 +37,6 @@ class ActivityAdmin(SummernoteModelAdmin):
         self.request = request
         return qs
 
-    def copy_clipboard_field(self, obj):
-        abs_url = self.request.build_absolute_uri(obj.absolute_url)
-        return mark_safe("""
-    {0} <a href="javascript:copyToClipboard('{0}');"> ─ Copiar &#128203;</a>
-    <script>
-    function copyToClipboard (str) {{
-    var dummy = document.createElement("input");
-      document.body.appendChild(dummy);
-      dummy.setAttribute("id", "dummy_id");
-      document.getElementById("dummy_id").value=str;
-      dummy.select();
-      document.execCommand("copy");
-      document.body.removeChild(dummy);
-      alert(str + ' copied.');
-    }}
-    </script>
-    """.format(abs_url))
-
-    copy_clipboard_field.short_description = 'Link a la sessió'
-
-    def copy_clipboard_list_field(self, obj):
-        abs_url = self.request.build_absolute_uri(obj.absolute_url)
-        return mark_safe("""
-    <a href="javascript:copyToClipboard('{0}');">Copiar &#128203;</a>
-    <script>
-    function copyToClipboard (str) {{
-    var dummy = document.createElement("input");
-      document.body.appendChild(dummy);
-      dummy.setAttribute("id", "dummy_id");
-      document.getElementById("dummy_id").value=str;
-      dummy.select();
-      document.execCommand("copy");
-      document.body.removeChild(dummy);
-      alert(str + ' copied.');
-    }}
-    </script>
-    """.format(abs_url))
-
-    copy_clipboard_list_field.short_description = 'Link a la sessió'
-
     def get_urls(self):
         urls = super().get_urls()
         custom_urls = [
