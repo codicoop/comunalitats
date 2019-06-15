b44d90e11b8ee95348ba32077f5b2b075c5a1a95
Pere <pere@perepicornell.com>
2019-06-15 22:07:37 +0200

    Exports: fixing DataExportCorrelation model name.

--- apps/dataexports/migrations/0002_auto_20190615_2107.py
+++ apps/dataexports/migrations/0002_auto_20190615_2107.py
@@ -11,7 +11,7 @@ class Migration(migrations.Migration):
 
     operations = [
         migrations.CreateModel(
-            name='DataExportsCorrelations',
+            name='DataExportsCorrelation',
             fields=[
                 ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
                 ('created', models.DateTimeField(auto_now_add=True, verbose_name='creació')),
