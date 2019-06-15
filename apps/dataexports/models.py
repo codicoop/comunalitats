1c559b6d56e3564bbf5226bbdb7a9993986990f6
Pere <pere@perepicornell.com>
2019-06-15 21:08:28 +0200

    Exports: added model for correlations. Fixed things in Exports model.

--- apps/dataexports/models.py
+++ apps/dataexports/models.py
@@ -9,9 +9,33 @@ class DataExports(models.Model):
         ordering = ["-subsidy_period"]
 
     created = models.DateTimeField(verbose_name="creació", auto_now_add=True)
-    subsidy_period = models.CharField("convocatòria", blank=True, null=True, max_length=4, default=2019,
-                                      choices=settings.SUBSIDY_PERIOD_OPTIONS)
+    subsidy_period = models.CharField(
+        "convocatòria", max_length=4, default=2019, choices=settings.SUBSIDY_PERIOD_OPTIONS)
     name = models.CharField("nom", max_length=200, unique=True)
     notes = models.TextField("apunts", blank=True, null=True)
     function_name = models.CharField("nom de la funció", max_length=150, unique=True,
                                      help_text="No modifiqueu aquesta dada.")
+
+    def __str__(self):
+        return self.name
+
+
+class DataExportsCorrelations(models.Model):
+    class Meta:
+        verbose_name = "correlació"
+        verbose_name_plural = "correlacions"
+        ordering = ["-subsidy_period"]
+
+    created = models.DateTimeField(verbose_name="creació", auto_now_add=True)
+    subsidy_period = models.CharField(
+        "convocatòria", max_length=4, default=2019, choices=settings.SUBSIDY_PERIOD_OPTIONS)
+    CORRELATED_FIELD_OPTIONS = (
+        ('Eix', 'Eix'),
+    )
+    correlated_field = models.CharField("camp", max_length=100, choices=CORRELATED_FIELD_OPTIONS)
+    original_data = models.CharField("dada original", max_length=200, help_text="Valor del camp al back-office.")
+    correlated_data = models.CharField("dada correlacionada", max_length=200,
+                                       help_text="Com ha de quedar el valor quan l'exportem per la justificació.")
+
+    def __str__(self):
+        return self.subsidy_period + ", " + self.correlated_field
