db8f075d70b04badfda05de2a85ae39481d47fe5
Pere <pere@perepicornell.com>
2019-08-02 16:16:52 +0200

    Exports: stage_type field now calculated and its correlations added.

--- apps/dataexports/export_functions.py
+++ apps/dataexports/export_functions.py
@@ -325,7 +325,7 @@ class ExportFunctions:
                 "destinatari?",  # "Destinatari de l'actuació" Opcions: Persona física/Promotor del projecte/Entitat <- d'on trec aquesta dada?
                 item.project.name,  # "En cas d'entitat (Nom de l'entitat)" <- aquí repetim el nom del projecte?
                 "Constituida",  # "En cas d'entitat" Opcions: Constituida/En procés/No finalitzat. Dada que ve del camp Estat del projecte.
-                "Nova creació",  # "Creació/consolidació". Opcions: Nova creació/Creixement. Ve del camp Tipus d'acompanyament.
+                cls.get_correlation("stage_type", item.stage_type),  # "Creació/consolidació".
                 item.date_start,
                 item.project.town,
                 item.project.object_finality,  # Breu descripció. Hi he posat el camp d'Objecte i finalitat. És OK?
