584f33b2c85de407997dab480aa0a06774e9f236
Pere <pere@perepicornell.com>
2019-08-12 13:49:14 +0200

    User model: dynamic value in Choices replaced for static text to avoid multi-tenancy problems (django wanting to create migrations for each different project name).

--- apps/coopolis/models.py
+++ apps/coopolis/models.py
@@ -181,7 +181,7 @@ class User(BaseUser):
     DISCOVERED_US_OPTIONS = (
         ('INTERNET', 'Per internet i xarxes socials'),
         ('FRIEND', "A través d'un conegut"),
-        ('PREVIOUS_ACTIVITY', "Per una activitat de "+settings.PROJECT_NAME),
+        ('PREVIOUS_ACTIVITY', "Per una activitat de l'ateneu"),
         ('OTHER', 'Altres')
     )
     discovered_us = models.TextField("com ens has conegut", blank=True, null=True, choices=DISCOVERED_US_OPTIONS)
