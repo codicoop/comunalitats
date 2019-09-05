83c3e7d3978f77f959d49c60008c990d937fe4d3
Pere <pere@perepicornell.com>
2019-09-05 13:53:55 +0200

    Fixing has_usable_password() issue. It's fixed since Django 2.1, but we had to rollback to 2.0, which caused that many users were not able to recover their passwords: https://docs.djangoproject.com/en/2.2/ref/contrib/auth/#django.contrib.auth.models.User.has_usable_password is_active filter also removed, given that we're not using that.
    This function can be removed as soon as we migrate to >2.0.

--- apps/coopolis/views/RegistrationMailView.py
+++ apps/coopolis/views/RegistrationMailView.py
@@ -35,6 +35,23 @@ class CustomPasswordResetForm(forms.PasswordResetForm):
         return super().send_mail(subject_template_name, email_template_name,
                                  context, from_email, to_email, html_email_template_name)
 
+    def get_users(self, email):
+        """Fixing has_usable_password() issue.
+        It's fixed since Django 2.1, but we had to rollback to 2.0, which caused that
+        many users were not able to recover their passwords:
+        https://docs.djangoproject.com/en/2.2/ref/contrib/auth/#django.contrib.auth.models.User.has_usable_password
+        is_active filter also removed, given that we're not using that.
+
+        This function can be removed as soon as we migrate to >2.0.
+        """
+        from django.contrib.auth import get_user_model
+        UserModel = get_user_model()
+
+        active_users = UserModel._default_manager.filter(**{
+            '%s__iexact' % UserModel.get_email_field_name(): email,
+        })
+        return (u for u in active_users)
+
 
 class CustomPasswordResetView(auth_views.PasswordResetView):
     form_class = CustomPasswordResetForm
