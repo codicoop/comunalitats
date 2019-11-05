3c34d30dd271908ab54beb0c3683353b3711008f
Pere <pere@perepicornell.com>
2019-11-05 15:10:12 +0100

    Preparing signal.

--- apps/cc_courses/apps.py
+++ apps/cc_courses/apps.py
@@ -5,3 +5,6 @@ from django.conf import settings
 class CoursesConfig(AppConfig):
     name = 'cc_courses'
     verbose_name = settings.COURSES_APP_TITLE
+
+    # def ready(self):
+    #     import cc_courses.signals
---
+++ apps/cc_courses/signals.py
@@ -0,0 +1,10 @@
+# from django.db.models.signals import post_delete
+# from django.dispatch import receiver
+#
+# from cc_courses.models import Activity
+#
+#
+# @receiver(post_delete, sender=Activity)
+# def delete_room_reservation(sender, instance, *args, **kwargs):
+#     # print('signal triggered!')
+#     pass
