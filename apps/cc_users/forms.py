94f03dbbc9ec8fe5eb102a1e2a19344a8e022da2
Pere <pere@perepicornell.com>
2019-11-20 11:12:14 +0100

    Front, profile: datepicker, district validation and hiding, field order and names, now correctly set.

--- apps/cc_users/forms.py
+++ apps/cc_users/forms.py
@@ -5,6 +5,9 @@ from django import forms
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm, UserChangeForm
 from django.contrib.auth import get_user_model
 
+from coopolis.widgets import XDSoftDatePickerInput
+from coopolis.mixins import FormDistrictValidationMixin
+
 
 class SignUpForm(UserCreationForm):
     first_name = forms.CharField(label="Nom", max_length=30, required=False, help_text='Opcional.')
@@ -31,13 +34,19 @@ class LogInForm(AuthenticationForm):
         return self.cleaned_data
 
 
-class MyAccountForm(UserChangeForm):
-
+class MyAccountForm(FormDistrictValidationMixin, UserChangeForm):
     class Meta:
         model = get_user_model()
-        fields = UserChangeForm.Meta.fields
-        exclude = ['password', 'is_confirmed', 'username', 'groups', 'user_permissions', 'is_staff', 'is_active',
-                   'is_superuser', 'last_login', 'date_joined', 'project']
+        fields = ['first_name', 'last_name', 'surname2', 'id_number', 'email', 'phone_number', 'birthdate',
+                  'birth_place', 'town', 'district', 'address', 'gender', 'educational_level',
+                  'employment_situation', 'discovered_us', ]
+
+    required_css_class = "required"
+    first_name = forms.CharField(label="Nom", max_length=30)
+    last_name = forms.CharField(label="Cognom", max_length=30, required=False)
+    email = forms.EmailField(
+        label="Correu electrònic", max_length=254, help_text='Requerit, ha de ser una adreça vàlida.')
+    birthdate = forms.DateField(label="Data de naixement", required=False, widget=XDSoftDatePickerInput())
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
