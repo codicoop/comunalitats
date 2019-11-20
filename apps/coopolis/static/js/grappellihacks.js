683d0df6d600479ef62c1472f57a84fcd633c3d0
Pere <pere@perepicornell.com>
2019-11-20 14:20:05 +0100

    Hack not needed anymore after updating django and grappelli.

--- apps/coopolis/static/js/grappellihacks.js
+++ apps/coopolis/static/js/grappellihacks.js
@@ -1,7 +1,3 @@
 grp.jQuery(function() {
     grp.jQuery('.hasDatepicker').datepicker('option', 'firstDay', 1);
 });
-
-grp.jQuery(function() {
-    grp.jQuery('.grp-remove-handler').parent().parent().parent().hide();
-});
