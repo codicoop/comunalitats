8eb580d20337f5df1528d8108e4d791febd0bf60
Pere <pere@perepicornell.com>
2020-05-05 11:13:36 +0200

    Front mostra botó i info de llista d'espera si té les places plenes.

--- apps/cc_courses/templatetags/enrollable.py
+++ apps/cc_courses/templatetags/enrollable.py
@@ -1,13 +1,11 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-
-from django.conf import settings
 from django import template
 
 
-def can_enroll(enrollable, individual):
-    return individual not in enrollable.enrolled.all() and enrollable.remaining_spots > 0
+def is_enrolled(enrollable, individual):
+    return individual in enrollable.enrolled.all()
 
 
 register = template.Library()
-register.filter('can_enroll', can_enroll)
+register.filter('is_enrolled', is_enrolled)
