63444c4c1c5e74af75e3cd53d71eeaa92e83311f
Pere <pere@perepicornell.com>
2020-05-06 10:55:01 +0200

    My Activities: mostrant bloc a banda per les que ets en llista d'espera.

--- apps/cc_courses/views/MyCoursesView.py
+++ apps/cc_courses/views/MyCoursesView.py
@@ -13,7 +13,9 @@ class MyCoursesListView(generic.ListView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['enrolled_activities'] = ActivityEnrolled.objects.filter(
-            activity__date_start__gte=timezone.now().date(), user=self.request.user)
+            activity__date_start__gte=timezone.now().date(), user=self.request.user, waiting_list=False)
+        context['waiting_list_activities'] = ActivityEnrolled.objects.filter(
+            activity__date_start__gte=timezone.now().date(), user=self.request.user, waiting_list=True)
         context['past_enrolled_activities'] = ActivityEnrolled.objects.filter(
             activity__date_start__lt=timezone.now().date(), user=self.request.user)
         return context
