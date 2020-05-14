b63d4985fe1bb23fde0ba1b4a21e8c846565ce9b
Pere <pere@perepicornell.com>
2020-05-14 11:54:56 +0200

    Problem in deploy with migration dependencies, making temporary script to bypass them.

--- docker-develop/sprint_may_migrates.sh
+++ docker-develop/sprint_may_migrates.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 echo "Running migrate develop_coopolis_app"
-docker exec develop_coopolis_app python manage.py migrate mail_queue
+docker exec develop_coopolis_app python manage.py migrate mailqueue
 docker exec develop_coopolis_app python manage.py migrate
 echo "Running migrate develop_coopolis_segon_ateneu"
-docker exec develop_coopolis_segon_ateneu python manage.py migrate mail_queue
+docker exec develop_coopolis_segon_ateneu python manage.py migrate mailqueue
 docker exec develop_coopolis_segon_ateneu python manage.py migrate
