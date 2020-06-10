614208cc5ec8f41752dc7194e89d83826998d9ef
Pere <pere@perepicornell.com>
2020-06-10 12:53:02 +0200

    Afegida fila de totals

--- apps/coopolis/admin/ProjectsFollowUpAdmin.py
+++ apps/coopolis/admin/ProjectsFollowUpAdmin.py
@@ -25,10 +25,10 @@ class ProjectsFollowUpAdmin(admin.ModelAdmin):
 
         query = {
             'members_h': Count('stages__involved_partners',
-                             filter=Q(stages__involved_partners__gender='MALE')),
+                             filter=Q(stages__involved_partners__gender='MALE'), distinct=True),
             'members_d': Count('stages__involved_partners',
-                               filter=Q(stages__involved_partners__gender='FEMALE')),
-            'members_total': Count('stages__involved_partners'),
+                               filter=Q(stages__involved_partners__gender='FEMALE'), distinct=True),
+            'members_total': Count('stages__involved_partners', distinct=True),
             'acollida_hores': Sum('stages__hours', filter=Q(stages__stage_type=1)),
             'acollida_certificat':
                 Count('stages__scanned_certificate',
@@ -74,25 +74,38 @@ class ProjectsFollowUpAdmin(admin.ModelAdmin):
         # Normally it should be easier to call aggregate to have the totals, but given how complex is it to combine
         # with the ORM filters, I opted for filling the values like this.
         # The original version was: qs.aggregate(**query)
-        #
-        # totals = dict(
-        #     turns=0,
-        #     total_to_pay=0,
-        #     total_grants_obtained=0,
-        #     total_to_pay_after_grants=0,
-        #     total_payments_without_grants=0,
-        #     to_pay=0
-        # )
-        # for row in response.context_data['rows']:
-        #     totals['turns'] += row.turns if row.turns else 0
-        #     totals['total_to_pay'] += row.total_to_pay if row.total_to_pay else 0
-        #     totals['total_grants_obtained'] += row.total_grants_obtained if row.total_grants_obtained else 0
-        #     totals['total_to_pay_after_grants'] += row.total_to_pay_after_grants if row.total_to_pay_after_grants else 0
-        #     totals['total_payments_without_grants'] += row.total_payments_without_grants \
-        #         if row.total_payments_without_grants else 0
-        #     totals['to_pay'] += row.to_pay if row.to_pay else 0
-        #
-        # response.context_data['totals'] = totals
+        totals = dict(
+            total_members_h=0,
+            total_members_d=0,
+            total_members_total=0,
+            total_acollida_hores=0,
+            total_acollida_certificat=0,
+            total_proces_hores=0,
+            total_proces_certificat=0,
+            total_constitucio_hores=0,
+            total_constitucio_certificat=0,
+            total_consolidacio_hores=0,
+            total_consolidacio_certificat=0,
+            total_employment_insertions=0,
+            total_constitutions=0,
+        )
+        for row in response.context_data['rows']:
+            totals['total_members_h'] += row.members_h if row.members_h else 0
+            totals['total_members_d'] += row.members_d if row.members_d else 0
+            totals['total_members_total'] += row.members_total if row.members_total else 0
+            totals['total_acollida_hores'] += row.acollida_hores if row.acollida_hores else 0
+            totals['total_acollida_certificat'] += 1 if row.acollida_certificat else 0
+            totals['total_proces_hores'] += row.proces_hores if row.proces_hores else 0
+            totals['total_proces_certificat'] += 1 if row.proces_certificat else 0
+            totals['total_constitucio_hores'] += row.constitucio_hores if row.constitucio_hores else 0
+            totals['total_constitucio_certificat'] += 1 if row.constitucio_certificat else 0
+            totals['total_consolidacio_hores'] += row.consolidacio_hores if row.consolidacio_hores else 0
+            totals['total_consolidacio_certificat'] += 1 if row.consolidacio_certificat else 0
+            totals['total_employment_insertions'] += \
+                len(row.employment_insertions.all()) if row.employment_insertions else 0
+            totals['total_constitutions'] += 1 if row.constitution_date else 0
+
+        response.context_data['totals'] = totals
 
         return response
 
