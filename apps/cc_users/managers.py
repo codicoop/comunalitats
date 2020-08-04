f7770abe0d7f0616c5a5bab40f5ed96563d14534
Pere <pere@perepicornell.com>
2020-08-04 20:14:22 +0200

    Forçant els e-mails a lowercase al desar-se. Fent el login case-insensitive per compatibilitat backwards amb els comptes que ja tenen l'e-mail guardat amb alguna majúscula.

--- apps/cc_users/managers.py
+++ apps/cc_users/managers.py
@@ -7,3 +7,14 @@ from django.contrib.auth.models import UserManager
 class CCUserManager(UserManager):
     def create_superuser(self, email, password, **extra_fields):
         return super().create_superuser(email, email, password, **extra_fields)
+
+    @classmethod
+    def normalize_email(cls, email):
+        """
+        Normalize the email address by lowercasing it.
+        """
+        return email.lower()
+
+    def get_by_natural_key(self, username):
+        username_field = f"{self.model.USERNAME_FIELD}__iexact"
+        return self.get(**{username_field: username})
