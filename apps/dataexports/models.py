72dec88e89ad4c9d50e5ce9005c344d0a91d2d00
Pere <pere@perepicornell.com>
2020-08-07 12:15:54 +0200

    Convocatòries: constraining que no puguin tenir dates solapades per evitar bugs amb funcions que esperen que només hi hagi una convocatòria per una data determinada.

--- apps/dataexports/models.py
+++ apps/dataexports/models.py
@@ -1,3 +1,4 @@
+from django.core.exceptions import ValidationError
 from django.db import models
 from django.utils.timezone import now
 
@@ -11,16 +12,46 @@ class SubsidyPeriod(models.Model):
     name = models.CharField("nom", max_length=250)
     date_start = models.DateField("dia d'inici")
     date_end = models.DateField("dia de finalització")
-    number = models.CharField("número d'expedient", max_length=50, null=True, blank=True)
+    number = models.CharField(
+        "número d'expedient", max_length=50, null=True, blank=True
+    )
 
     @property
     def range(self):
-        return (self.date_start, self.date_end)
+        return self.date_start, self.date_end
 
     @staticmethod
     def get_current():
         today = now()
-        return SubsidyPeriod.objects.get(date_start__lte=today, date_end__gte=today)
+        return SubsidyPeriod.objects.get(
+            date_start__lte=today, date_end__gte=today
+        )
+
+    def clean(self):
+        super().clean()
+        """
+        Prevent the selected period from overlapping the one of any other.
+        """
+        q = SubsidyPeriod.objects.filter(
+            models.Q(
+                date_start__gte=self.date_start, date_start__lt=self.date_end
+            )
+            | models.Q(
+                date_end__gt=self.date_start, date_end__lte=self.date_end
+            )
+            | models.Q(
+                date_start__lte=self.date_start, date_end__gte=self.date_end
+            )
+        )
+
+        if self.id:
+            q = q.exclude(id=self.id)
+
+        if q.count() > 0:
+            err = ("La data d'inici i/o la de finalització seleccionades "
+                   "trepitgen les d'alguna altra convocatòria, selecciona'n "
+                   "unes altres.")
+            raise ValidationError({'date_start': err, 'date_end': err})
 
     def __str__(self):
         return self.name
