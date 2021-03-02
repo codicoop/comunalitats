9c7d1dbaf44720e83d3ba24b2d8fb466c51a70f1
Pere <pere@perepicornell.com>
2021-03-02 14:50:52 +0100

    HomeView: netejat de variables en desús.

--- apps/coopolis/views/HomeView.py
+++ apps/coopolis/views/HomeView.py
@@ -1,14 +1,5 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-
 from coopolis.views import LoginSignupContainerView
 
 
 class HomeView(LoginSignupContainerView):
     template_name = "home.html"
-    extra_context = {
-        'courses_title': "Formació i activitats",
-        'courses_text': "TEXT D'INTRODUCCIÓ A LES FORMACIONS QUE FEM",
-        'projects_title': "Acompanyament de projectes",
-        'projects_text': "TEXT D'INTRODUCCIÓ A L'ACOMPANYAMENT DE PROJECTES"
-    }
