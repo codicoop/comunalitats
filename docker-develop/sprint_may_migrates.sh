0bf59bad878b9ce4d6d4ace9cb2180b03192e490
Pere <pere@perepicornell.com>
2021-04-07 15:18:34 +0200

    Cleaning up one-time migration script

--- docker-develop/sprint_may_migrates.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-echo "Running migrate develop_coopolis_app"
-docker exec develop_coopolis_app python manage.py migrate mailqueue
-docker exec develop_coopolis_app python manage.py migrate mailing_manager
-docker exec develop_coopolis_app python manage.py migrate
-echo "Running migrate develop_coopolis_segon_ateneu"
-docker exec develop_coopolis_segon_ateneu python manage.py migrate mailqueue
-docker exec develop_coopolis_segon_ateneu python manage.py migrate mailing_manager
-docker exec develop_coopolis_segon_ateneu python manage.py migrate
