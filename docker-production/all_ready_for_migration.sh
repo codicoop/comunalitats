25d1195c0e6973b2fa97f53e647d52e32521ca1e
Pere <pere@perepicornell.com>
2021-04-08 13:43:22 +0200

    All ready for migration updated.

--- docker-production/all_ready_for_migration.sh
+++ docker-production/all_ready_for_migration.sh
@@ -1,13 +1,22 @@
 #!/bin/bash
-docker exec ateneu_catcentral python manage.py stage_sessions_report --check
-docker exec ateneu_vallesoccidental python manage.py stage_sessions_report --check
-docker exec ateneu_bnord python manage.py stage_sessions_report --check
-docker exec ateneu_ponentcoopera python manage.py stage_sessions_report --check
-docker exec ateneu_coopolis python manage.py stage_sessions_report --check
-docker exec ateneu_coopmaresme python manage.py stage_sessions_report --check
-docker exec ateneu_coopcamp python manage.py stage_sessions_report --check
-docker exec ateneu_coopsetania python manage.py stage_sessions_report --check
-docker exec ateneu_terresgironines python manage.py stage_sessions_report --check
-docker exec ateneu_hospitalet python manage.py stage_sessions_report --check
-docker exec ateneu_terresebre python manage.py stage_sessions_report --check
-docker exec ateneu_altpirineu python manage.py stage_sessions_report --check
+declare -A ateneus=(
+  ["ateneu_catcentral"]="ateneus_catcentral"
+  ["ateneu_vallesoccidental"]="ateneus_vallesoccidental"
+  ["ateneu_bnord"]="ateneus_bnord"
+  ["ateneu_ponentcoopera"]="ateneus_ponentcoopera"
+  ["ateneu_coopolis"]="coopolis"
+  ["ateneu_coopmaresme"]="ateneus_coopmaresme"
+  ["ateneu_coopcamp"]="ateneus_coopcamp"
+  ["ateneu_coopsetania"]="ateneus_coopsetania"
+  ["ateneu_terresgironines"]="ateneus_terresgironines"
+  ["ateneu_hospitalet"]="ateneus_hospitalet"
+  ["ateneu_terresebre"]="ateneus_terresebre"
+  ["ateneu_altpirineu"]="ateneus_altpirineu"
+)
+
+for ateneu in "${!ateneus[@]}"
+do
+  :
+  printf "Running stage_sessions_report --migrate to: %s\n" "$ateneu"
+  docker exec "$ateneu" python manage.py stage_sessions_report --check
+done
