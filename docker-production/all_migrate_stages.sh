01ef1e8184c711a176398c91085dec64bfaf2091
Pere <pere@perepicornell.com>
2021-04-08 13:42:46 +0200

    All stages migration updated.

--- docker-production/all_migrate_stages.sh
+++ docker-production/all_migrate_stages.sh
@@ -1,26 +1,22 @@
 #!/bin/bash
-echo "Running migration for ateneu_catcentral"
-docker exec ateneu_catcentral python manage.py stage_sessions_report --migrate > /srv/reports/ateneu_catcentral.html
-echo "Running migration for ateneu_vallesoccidental"
-docker exec ateneu_vallesoccidental python manage.py stage_sessions_report --migrate > /srv/reports/ateneu_vallesoccidental.html
-echo "Running migration for ateneu_bnord"
-docker exec ateneu_bnord python manage.py stage_sessions_report --migrate > /srv/reports/ateneu_bnord.html
-echo "Running migration for ateneu_ponentcoopera"
-docker exec ateneu_ponentcoopera python manage.py stage_sessions_report --migrate > /srv/reports/ateneu_ponentcoopera.html
-echo "Running migration for ateneu_coopolis"
-docker exec ateneu_coopolis python manage.py stage_sessions_report --migrate > /srv/reports/ateneu_coopolis.html
-echo "Running migration for ateneu_coopmaresme"
-docker exec ateneu_coopmaresme python manage.py stage_sessions_report --migrate > /srv/reports/ateneu_coopmaresme.html
-echo "Running migration for ateneu_coopcamp"
-docker exec ateneu_coopcamp python manage.py stage_sessions_report --migrate > /srv/reports/ateneu_coopcamp.html
-echo "Running migration for ateneu_coopsetania"
-docker exec ateneu_coopsetania python manage.py stage_sessions_report --migrate > /srv/reports/ateneu_coopsetania.html
-echo "Running migration for ateneu_terresgironines"
-docker exec ateneu_terresgironines python manage.py stage_sessions_report --migrate > /srv/reports/ateneu_terresgironines.html
-echo "Running migration for ateneu_hospitalet"
-docker exec ateneu_hospitalet python manage.py stage_sessions_report --migrate > /srv/reports/ateneu_hospitalet.html
-echo "Running migration for ateneu_terresebre"
-docker exec ateneu_terresebre python manage.py stage_sessions_report --migrate > /srv/reports/ateneu_terresebre.html
-echo "Running migration for ateneu_altpirineu"
-docker exec ateneu_altpirineu python manage.py stage_sessions_report --migrate > /srv/reports/ateneu_altpirineu.html
-echo "Tots els stage_sessions_report --migrate > /srv/reports/ateneu_catcentral.htmls tirats, excepte ateneu_demo."
+declare -A ateneus=(
+  ["ateneu_catcentral"]="ateneus_catcentral"
+  ["ateneu_vallesoccidental"]="ateneus_vallesoccidental"
+  ["ateneu_bnord"]="ateneus_bnord"
+  ["ateneu_ponentcoopera"]="ateneus_ponentcoopera"
+  ["ateneu_coopolis"]="coopolis"
+  ["ateneu_coopmaresme"]="ateneus_coopmaresme"
+  ["ateneu_coopcamp"]="ateneus_coopcamp"
+  ["ateneu_coopsetania"]="ateneus_coopsetania"
+  ["ateneu_terresgironines"]="ateneus_terresgironines"
+  ["ateneu_hospitalet"]="ateneus_hospitalet"
+  ["ateneu_terresebre"]="ateneus_terresebre"
+  ["ateneu_altpirineu"]="ateneus_altpirineu"
+)
+
+for ateneu in "${!ateneus[@]}"
+do
+  :
+  printf "Running stage_sessions_report --migrate to: %s\n" "$ateneu"
+  docker exec "$ateneu" python manage.py stage_sessions_report --migrate > /srv/reports/stages_migration_"$ateneu".html
+done
