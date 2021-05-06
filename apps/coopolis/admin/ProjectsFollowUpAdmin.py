0023bea53e57648b7e9fed2833221d7c8f1957f1
Pere <pere@perepicornell.com>
2021-05-06 13:36:33 +0200

    Removing unused import

--- apps/coopolis/admin/ProjectsFollowUpAdmin.py
+++ apps/coopolis/admin/ProjectsFollowUpAdmin.py
@@ -11,7 +11,7 @@ from openpyxl.utils import get_column_letter
 
 from .ProjectAdmin import FilterByFounded
 from dataexports.models import SubsidyPeriod
-from coopolis.models.projects import ProjectStage, ProjectStageSession
+from coopolis.models.projects import ProjectStage
 from ..models import User
 
 
