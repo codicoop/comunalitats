8859fb9d1ec7a4e1c274b709aca35ae076aadf28
Pere <pere@perepicornell.com>
2022-01-12 12:12:35 +0100

    Sessions: Organizer eliminat dels filtres d'admin i Cercle afegit

--- src/apps/coopolis/admin/ActivityAdmin.py
+++ src/apps/coopolis/admin/ActivityAdmin.py
@@ -128,7 +128,7 @@ class ActivityAdmin(SummernoteModelAdminMixin, modelclone.ClonableModelAdmin):
     search_fields = ('date_start', 'name', 'objectives',)
     list_filter = (
         FilterBySubsidyPeriod,
-        'course', 'date_start', 'room', 'organizer', 'entity', 'axis', 'place',
+        'course', 'date_start', 'room', 'circle', 'entity', 'axis', 'place',
         'for_minors', 'cofunded',)
     fieldsets = [
         (None, {
