f0fb60ab1d589738cee470a1e24e8274b553a2b4
Pere Picornell <pere@perepicornell.com>
2022-07-18 10:31:15 +0200

    Eliminant scripts i dockerització de producció. Ajustant noms i scripts per develop.

--- docker/scripts/production/all_exportdbs.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-source sh_vars.conf
-
-for ateneu in "${!comunalitats[@]}"
-do
-  :
-  docker exec -i "$postgres_container" pg_dump "${comunalitats[$ateneu]}" -U postgres > "$dumps_path"/"${comunalitats[$ateneu]}".sql
-  printf "Exportada base de dades de: %s\n" "$ateneu"
-done
--- docker/scripts/production/all_migrates.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-source sh_vars.conf
-
-for ateneu in "${!comunalitats[@]}"
-do
-  :
-  printf "Running migrate %s\n" "$ateneu"
-  docker exec "$ateneu" python manage.py migrate
-done
