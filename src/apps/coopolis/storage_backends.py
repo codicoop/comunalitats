8f30de54eaa3905574fb58c3a9bc4de4c5dde019
Pere Picornell <pere@perepicornell.com>
2022-07-27 11:11:19 +0200

    Ara els uploads en PublicMediaStorage no assumeixen que la config per defecte serà públic sinó que l'specifica. De manera que podem canviar els settings per fer que per defecte els fitxers siguin privats.

--- src/apps/coopolis/storage_backends.py
+++ src/apps/coopolis/storage_backends.py
@@ -6,11 +6,12 @@ from storages.backends.s3boto3 import S3Boto3Storage
 
 class PublicMediaStorage(S3Boto3Storage):
     location = settings.AWS_PUBLIC_MEDIA_LOCATION
+    default_acl = "public-read"
     file_overwrite = False
 
 
 class PrivateMediaStorage(S3Boto3Storage):
     location = settings.AWS_PRIVATE_MEDIA_LOCATION
-    default_acl = 'private'
+    default_acl = "private"
     file_overwrite = False
     custom_domain = False
