5e8e2606ed2ea25f12fad764f449a031278cbf3d
Pere Picornell <pere@perepicornell.com>
2022-11-21 18:03:38 +0100

    Admin de polls mogut a la seva app

--- src/apps/coopolis/admin/__init__.py
+++ src/apps/coopolis/admin/__init__.py
@@ -1,10 +1,7 @@
 from django.contrib import admin
 
-from apps.polls.models import ActivityPoll
 from apps.cc_users.models import User
 from .UserAdmin import UserAdmin
-from .ActivityPollAdmin import ActivityPollAdmin
 
 
 admin.site.register(User, UserAdmin)
-admin.site.register(ActivityPoll, ActivityPollAdmin)
