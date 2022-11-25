4d0c59fc82b41ce22c5cdb9aab5fd3d38ab88cc2
Pere Picornell <pere@perepicornell.com>
2022-11-25 15:03:35 +0100

    typo

--- src/apps/cc_courses/models.py
+++ src/apps/cc_courses/models.py
@@ -364,8 +364,8 @@ class Activity(models.Model):
         return obj
 
     def poll_access_allowed(self):
-         # Si la data actual és superior o igual a la data i hora d'inici,
-        # mostrem  l'enquesta.
+        # Si la data actual és superior o igual a la data i hora d'inici,
+        # mostrem l'enquesta.
         naive_datetime = datetime.combine(self.date_start, self.starting_time)
         aware_datetime = timezone.make_aware(naive_datetime)
         if timezone.now() >= aware_datetime:
