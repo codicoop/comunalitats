2ef7d14d65957891bdca9b834b504a9c76ba7d2d
eliashz <eliashz@gmail.com>
2024-04-30 17:47:42 +0200

    #152 Crear punto de API y lógica JS

--- src/apps/coopolis/static/js/chained_dropdown.js
+++ src/apps/coopolis/static/js/chained_dropdown.js
@@ -1,5 +1,10 @@
 jQuery(function($){
     $(document).ready(function(){
+        var cloneService = document.getElementById("id_service").cloneNode(true);
+        $("#id_subsidy_period").change(function(){
+           update_services($(this).val(), cloneService); 
+        });
+        update_services($("#id_service").val(), cloneService)
         var clone = document.getElementById("id_sub_service").cloneNode(true);
         $("#id_service").change(function(){
             update_sub_services($(this).val(), clone)
@@ -7,6 +12,31 @@ jQuery(function($){
         update_sub_services($("#id_service").val(), clone)
     });
 
+    function update_services(subsidy_period, clone) {
+        $.ajax({
+            url:"/chained_dropdowns/get_subsidy_period/",
+            type:"GET",
+            data:{subsidy_period: subsidy_period,},
+            success: function(result) {
+                var cols = document.getElementById("id_service");
+                cols.innerHTML = clone.innerHTML
+                Array.from(cols.options).forEach(function(option_element) {
+                    var existing = false;
+                    for (var k in result) {
+                        if (option_element.value == k || option_element.value == "") {
+                            existing = true
+                        }
+                    }
+                    if (existing == false) {
+                        $("#id_service option[value='"+option_element.value+"']").remove();
+                    }
+                })
+            },
+            error: function(e){
+                console.error(JSON.stringify(e));
+            },
+        });
+    }
     function update_sub_services(service, clone) {
         $.ajax({
             url:"/chained_dropdowns/get_sub_services/",
--- src/apps/coopolis/views/ChainedDropdownView.py
+++ src/apps/coopolis/views/ChainedDropdownView.py
@@ -17,3 +17,24 @@ def get_sub_services(request):
             item.value: item.label for item in service.get_sub_services()
         }
     return JsonResponse(data=sub_services, safe=False)
+
+
+@login_required
+def get_subsidy_period(request):
+    subsidy_period = request.GET.get("subsidy_period")
+    item_start = ServicesChoices.A
+    item_end = ServicesChoices.E
+    # !TODO: revisar
+    if subsidy_period == "1":
+        item_start = ServicesChoices.F
+        item_end = ServicesChoices.J
+
+    if not subsidy_period:
+        services = None
+    elif subsidy_period:
+        services = {
+            item.value: item.label
+            for item in ServicesChoices
+            if item in range(item_start, item_end + 1)
+        }
+    return JsonResponse(data=services, safe=False)
