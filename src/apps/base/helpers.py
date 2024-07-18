83395ed6e3c17816d55bd7fa7afecda38ca330ac
Pere Picornell <pere@perepicornell.com>
2024-07-18 15:22:47 +0200

    SObrava el punt al helper

--- src/apps/base/helpers.py
+++ src/apps/base/helpers.py
@@ -3,6 +3,6 @@ def add_justification_required_text_to_field(
         text="Dada necessària per la justificació.",
 ):
     if hasattr(field, 'help_text') and field.help_text:
-        text = f"{field.help_text}. {text}"
+        text = f"{field.help_text} {text}"
     field.help_text = text
     return field
